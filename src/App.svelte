0b7c489874eb230c12447ab268051812ba871283
Julian-Mostbauer <julian.mostbauer@outlook.com>
2024-08-03 23:08:47 +0200

    fabric test

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,34 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import { onMount } from "svelte";
+
+  function drawSquare() {
+    const canvas = new fabric.Canvas("c");
+    let rect = new fabric.Rect({
+      left: 120,
+      top: 10,
+      fill: "blue",
+      width: 150,
+      height: 150,
+      angle: 45,
+    });
+    canvas.add(rect);
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<svelte:head>
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/500/fabric.min.js"
+  ></script>
+</svelte:head>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<div>
+  <canvas class="main-canvas" id="c" on:click={drawSquare}></canvas>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .main-canvas {
+    width: 250px;
+    height: 250px;
+    border: 1px solid #000000;
   }
 </style>
