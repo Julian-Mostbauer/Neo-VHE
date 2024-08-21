05d5eacf8653c43fcbc3584c4ecd11550b0d80a0
Julian-Mostbauer <julian.mostbauer@outlook.com>
2024-08-21 11:51:43 +0200

    made it so you can edit points in the polygon

--- src/lib/PolygonOptions.svelte
+++ src/lib/PolygonOptions.svelte
@@ -16,6 +16,41 @@
     fill:
     <input type="color" bind:value={$shapeStore.fill} placeholder="Fill" />
   </label>
+
+  <label>
+    points:
+    <div class="point-table-container">
+      <table class="point-table">
+        <thead>
+          <tr>
+            <th>X</th>
+            <th>Y</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each $shapeStore.points as point, index}
+            <tr>
+              <td>
+                <input
+                  type="number"
+                  bind:value={$shapeStore.points[index].x}
+                  placeholder="X"
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  bind:value={$shapeStore.points[index].y}
+                  placeholder="Y"
+                />
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </label>
+
   <label>
     HTML Element:
     <select bind:value={$shapeStore.htmlElement}>
